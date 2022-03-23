fa7521f0b80c217576411f24d4bbd71649fc3faf
thandar <thandar@nexbe.sg>
2022-03-23 23:02:19 +0630

    RangeRandom1

--- src/component/RangeRandom.js
+++ src/component/RangeRandom.js
@@ -1,29 +1,48 @@
-import {useState} from 'react'
-
+import {useEffect, useState} from 'react'
+import './RangeRandom.css';
 export const RangeRandom = () => {
     const[startVal,setStartVal] = useState("");
-    const[endtVal,setEndVal] = useState("");
+    const[endVal,setEndVal] = useState("");
     const[randomNumber,setRandomNumber] = useState("");
-    const resultRandomNumber = () =>{
-        setRandomNumber(Math.floor(Math.random()*(endtVal-startVal)*startVal));
+    const resultRandomNumber = (start,end) =>{
+        return Math.round(Math.random()*(end-start)+start)
     };
-  return (
-    <div className='RangeRandom'>
-    <div className='container'>
+    useEffect ( () => {
+        console.log("start value is", startVal)
+        console.log("end value is", endVal )
+    }, [startVal,endVal] )
+    return (
+        <div className='RangeRandom'>
+        <div className='container'>
         <div className='numContainer'>
             <div>
-            <p>Start</p>
-            <input type="number" value={startVal} onChange ={e => setStartVal(e.target.value)}/>
+                <label htmlFor = "start-input">Start</label>
+                <input name="start-input" type="number" value={startVal} 
+                onChange ={(e) => setStartVal(parseInt(e.target.value))}
+                min ={0}
+                />
             </div>
             <div>
-            <p>End</p>
-            <input type="number" value={endtVal} onChange ={e => setEndVal(e.target.value)}/>
+                <label htmlFor = "end-input">end</label>
+                <input name="end-input" type="number" value={ endVal } 
+                onChange ={(e) => setEndVal(parseInt(e.target.value))}
+                min ={startVal && startVal + 1}
+                />
             </div>
         </div><br/>
-        <button onClick={resultRandomNumber}>Range Random</button><br/>
+        <button onClick={ () => setRandomNumber(resultRandomNumber(startVal,endVal))}
+            disabled ={ !startVal || !endVal || endVal<= startVal}
+        >
+            Randomize
+        </button><br/>
         <div className='randomNumber'>
-            <p>Result </p>
-            <input type="number" value={randomNumber}></input>
+            <p>Result : {""}
+                <span className={
+                    randomNumber===startVal || randomNumber===endVal ? "bold-text" : ""
+                }>
+                    {randomNumber}
+                 </span>
+            </p>
         </div>
     </div>
     </div>
